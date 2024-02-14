746ba08373f5378ca9ef0a28e878e58c678ed023
Earthweb <107529899+Earthwweb@users.noreply.github.com>
2024-02-14 06:11:57 +0545

    Top article section created with top-viewed article

--- resources/views/components/base/front/top-news.blade.php
+++ resources/views/components/base/front/top-news.blade.php
@@ -1,11 +1,22 @@
 <div>
-
+    <div class="w-100 mb-2">
     @if ($article->image)
-                    <img height="w-100" src="{{ asset('storage/images/articles/'.$article->image) }}"  alt="{{ $article->title }}" />
+                    <img class="w-100" src="{{ asset('storage/images/articles/'.$article->image) }}"  alt="{{ $article->title }}" />
                     @endif
+    </div>
+    <div><span class="bg-primary text-white p-1">{{ $article->category->title }}</span></div>
 
-                    <h1>{{ $article->title}}</h1>
+    <div class="mt-2">
+        <h2 class="fw-bolder display-6" style="text-align:justify">
+            {{ $article->title}}
 
+        </h2>
+    </div>
 
-    {{ $article->content }}
+    <div class="d-flex justify-content-start gap-2">
+        <div>{{ $article->created_at->isoFormat("MMMM D, YYYY") }}</div>
+        <div>|</div>
+        <div>{{ $article->views }} Views</div>
+
+    </div>
 </div>
